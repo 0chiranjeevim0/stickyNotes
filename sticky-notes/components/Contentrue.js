b665554313ca2bb9866101884e4aadb9c834fe96
LENOVO <thehowtodeveloper@gmail.com>
2022-07-12 00:14:28 +0530

    user Auth

---
+++ sticky-notes/components/Contentrue.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Content.module.css';
+
+const Contentrue = () =>{
+    return(
+        <div className={styles.contentMainContainer}>
+
+        </div>
+    )
+} 
+
+export default Contentrue;
